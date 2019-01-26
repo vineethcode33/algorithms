24979e04ba2e74194a374a7bb0d91e318f63e41e
vmenneni <vmenneni@paypal.com>
2019-01-26 14:50:19 -0800

    hastable 1 solution

---
+++ CTCI/chapter1/is_unique_1_1.py
@@ -0,0 +1,26 @@
+"""
+Page # 90
+1.1 Is Unique: Implement an algorithm to determine if a string has all unique
+characters. What if you cannot use additional data structures?
+
+"""
+
+def unique(given_string):
+
+  # ASCII characters 128 starting from 000 -> 127
+  if len(given_string)>128:
+    return False
+
+  char_set = [False for _ in range(128)]
+
+  for char in given_string:
+    val = ord(char)
+    if char_set[val]:
+      return False
+    char_set[val] = True
+
+  return True
+
+if __name__ == "__main__":
+  print(unique("dfahiuysdgfagfui"))
+  print(unique("abcdefghijklmnopqrstuvwxyz"))
