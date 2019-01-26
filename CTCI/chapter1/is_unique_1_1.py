bf36015801d27b25ce973fb1cb85a93eda768bd1
vmenneni <vmenneni@paypal.com>
2019-01-26 14:59:35 -0800

    adding alternate approach

--- CTCI/chapter1/is_unique_1_1.py
+++ CTCI/chapter1/is_unique_1_1.py
@@ -5,22 +5,32 @@ characters. What if you cannot use additional data structures?
 
 """
 
+
 def unique(given_string):
 
-  # ASCII characters 128 starting from 000 -> 127
-  if len(given_string)>128:
-    return False
+    # ASCII characters 128 starting from 000 -> 127
+    if len(given_string) > 128:
+        return False
 
-  char_set = [False for _ in range(128)]
+    char_set = [False for _ in range(128)]
 
-  for char in given_string:
-    val = ord(char)
-    if char_set[val]:
-      return False
-    char_set[val] = True
+    for char in given_string:
+        val = ord(char)
+        if char_set[val]:
+            return False
+        char_set[val] = True
 
-  return True
+    return True
+
+
+def unique2(given_string):
+    return len(given_string) == len(set(given_string))
+
 
 if __name__ == "__main__":
-  print(unique("dfahiuysdgfagfui"))
-  print(unique("abcdefghijklmnopqrstuvwxyz"))
+    print(unique("dfahiuysdgfagfui"))
+    print(unique("abcdefghijklmnopqrstuvwxyz"))
+
+    # alternate approach
+    print(unique2("dfahiuysdgfagfui"))
+    print(unique2("abcdefghijklmnopqrstuvwxyz"))
